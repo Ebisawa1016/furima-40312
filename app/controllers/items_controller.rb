65990a8ff263416a1666a34b802a3628a9fcfe28
Ebisawa1016 <ebisawa.yusuke1016@gmail.com>
2024-02-13 18:42:39 +0900

    カラムの追加とActive Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,10 @@ class ItemsController < ApplicationController
   #def show
   #end
 
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
--- db/migrate/20240207082429_add_last_name_kanji_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLastNameKanjiToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :last_name_kanji, :string
-  end
-end
